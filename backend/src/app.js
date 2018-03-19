154662abc833040ae115dcde0bed562d55f9ff22
GA1 <nvkga1@gmail.com>
2018-03-19 12:44:48 +0100

    Create express hello world

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,13 @@
 var express = require('express');
 
+var app = express();
+
 app.use(express.static('public'));
+
+app.get('/api/v1/images', function (req, res) {
+  res.json("hello")
+});
+
 
 app.listen(4000, function () {
   console.log('Example app listening on port 4000!');

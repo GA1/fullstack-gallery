ad4e5e9de95422ca614d9ded033126b071f92fed
GA1 <nvkga1@gmail.com>
2018-03-19 13:57:35 +0100

    And api/v1/randomImages endpoint returning 50 random images

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,23 @@ app.get('/api/v1/images', function (req, res) {
   res.json(numbers.map(n => n*2))
 });
 
+app.get('/api/v1/images/randomImages', function (req, res) {
+  var img1 = {
+    'url': 'https://s3.eu-central-1.amazonaws.com/react-gallery/example_image2.jpg',
+  };
+  var img2 = {
+    'url': 'https://s3.eu-central-1.amazonaws.com/react-gallery/example_image.jpg',
+  };
+  var result = []
+  for (var i = 0; i < 50; i++) {
+    if (Math.random() < 0.5 )
+      result.push(img1)
+    else
+      result.push(img2)
+  }
+  res.json(result);
+});
+
 app.use('/', express.static('public'));
 
 app.listen(4000, function () {
